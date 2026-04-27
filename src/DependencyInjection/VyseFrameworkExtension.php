fd9e82ec9d458fe80c99bf599dffbb9c2ca4ba50
= <andy@prestigedigital.com>
2026-04-27 15:13:46 +0100

    Symfony Bundle

---
+++ src/DependencyInjection/VyseFrameworkExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vyse\Framework\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class VyseFrameworkExtension extends Extension
+{
+    public function load(
+        array $configs,
+        ContainerBuilder $container,
+    ): void {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../../config'),
+        );
+
+        $loader->load('vyse.yaml');
+    }
+}
---
+++ src/VyseFrameworkBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vyse\Framework;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VyseFrameworkBundle extends Bundle
+{
+}
